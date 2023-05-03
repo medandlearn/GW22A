65eb7e66c4cf63b3576d4e2992d3b60e57c2fbdd
medandlearn <gertikohlruss@gmail.com>
2023-05-03 20:34:52 +0000

    ToDoliste index.html und style.css erstellt

--- ErsteWebseite/BruttoPreisRechner/script.js
+++ ErsteWebseite/BruttoPreisRechner/script.js
@@ -1,13 +1,17 @@
 const frmBruttorechner = window.document.bruttopreisrechner;
+const inpMwst = frmBruttorechner.mwst;
+const inpNettoPreis = frmBruttorechner.nettopreis;
+
+
+
 const btnBerechnen = frmBruttorechner.berechnen;
 const ausgabe = document.querySelector('#id-ausgabe');
-console.log(frmBruttorechner.mwst.value);
-btnBerechnen.addEventListener("click", fBruttoPreisBerechnen);
 
+btnBerechnen.addEventListener("click", fBruttoPreisBerechnen);
 
 function fBruttoPreisBerechnen(event) {
     event.preventDefault();
-    const nettoPreis = parseFloat(frmBruttorechner.nettopreis.value);
-    const mwst = parseFloat(frmBruttorechner.mwst.value);
+    const nettoPreis = parseFloat(inpNettoPreis.value);
+    const mwst = parseFloat(inpMwst.value);
     ausgabe.innerHTML = (1+mwst/100)*nettoPreis;
 }
